0acbc95dafc306259b969b92ed506d1139b25db4
isanket87 <isanket787@gmail.com>
2026-03-30 18:27:46 +0530

    chore: update deployment and setup scripts for new server migration

--- patch-backup.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = '/var/www/brioright/server/scripts/backup.sh';
-let content = fs.readFileSync(path, 'utf8');
-
-content = content.replace(/DB_NAME=".*"/, 'DB_NAME="brioright"');
-content = content.replace(/DB_USER=".*"/, 'DB_USER="brioright"');
-
-fs.writeFileSync(path, content);
-console.log('Backup script updated successfully');
